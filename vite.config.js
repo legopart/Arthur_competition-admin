a79ac315780b52f0fddbd2ecab708f0ff2d4a68a
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2022-12-22 02:17:54 +0200

    update 5

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
       usePolling: true,
     },
     host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
+    strictPort: false,
+
   },
   build: {
     chunkSizeWarningLimit: 4600,
